a9452e3f66be31c8afc95b64db2e3200a94eb97b
Nahu88 <meliannahu@gmail.com>
2025-09-30 19:29:21 -0300

    Punto 3 terminado

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const frutasTienda = [
     {id:1, nombre:"Anana", precio:100, ruta_img:"img/anana.jpg", cantidad:0},
     {id:2, nombre:"Arandano", precio:80, ruta_img:"img/arandano.jpg", cantidad:0},
     {id:3, nombre:"Banana", precio:120, ruta_img:"img/banana.jpg", cantidad:0},
-    {id:4, nombre:"Frambuesa", precio:90, ruta_img:"img/frambuesa.jpg", cantidad:0},
+    {id:4, nombre:"Frambuesa", precio:90, ruta_img:"img/frambuesa.png", cantidad:0},
     {id:5, nombre:"Frutilla", precio:300, ruta_img:"img/frutilla.jpg", cantidad:0},
     {id:6, nombre:"Kiwi", precio:250, ruta_img:"img/kiwi.jpg", cantidad:0},
     {id:7, nombre:"Mandarina", precio:200, ruta_img:"img/mandarina.jpg", cantidad:0},
@@ -31,10 +31,54 @@ function imprimirDatosAlumno() {
 }
 
 
+//VARIABLES DEL DOM
+// Variables que conectan con los elementos del DOM 
+// Como ejemplo la barra de busqueda, los contenedores, los botones de ordenamientos , etc
+
+
+let carrito = [];
+
+const barraBusqueda = document.getElementById("barra-busqueda");
+
+// contenedores
+const contenedorProductos = document.getElementById("contenedor-productos");
+const contenedorCarrito = document.getElementById("contenedor-carrito");
+
+
+// Ver despues los de contador
+
+//Botones del ordenamiento
+const botonOrdenarNombre = document.getElementById("ordenar-nombre");
+const botonOrdenarPrecio = document.getElementById("ordenar-precio");
+
+
+//PUNTO 3
+// Es una funcion que va a recorrer un array de frutas y genera cards de html.
+// Cada card tiene su imagen, nombre, precio y  un boton para agregar al carrito.
+// los datos estan en el arrays de objetos de la frutasTienda
+
+
+function mostrarLista(array){
+
+  let htmlProductos = "";
+  array.forEach(fruta => {
+    htmlProductos += `
+    <div class="card-producto">
+      <img src="${fruta.ruta_img}" alt="${fruta.nombre}">
+      <h3>${fruta.nombre}</h3>
+      <p>$${fruta.precio}</p>
+      <button>Agregar al carrito</button>
+    </div>
+    `
+  });
+  contenedorProductos.innerHTML = htmlProductos;
+
+}
 
 
 function init(){
   imprimirDatosAlumno();
+  mostrarLista(frutasTienda)
 }
 
 init();
